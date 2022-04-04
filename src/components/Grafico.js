dca6a246cc114b76da0af9ea723bdecf3c827aeb
facuuguzman00 <facu11tuc@gmail.com>
2022-04-04 12:12:59 -0300

    graphic

--- src/components/Grafico.js
+++ src/components/Grafico.js
@@ -8,9 +8,9 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import faker from 'faker';
+  
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
 
 ChartJS.register(
   CategoryScale,
@@ -19,10 +19,9 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
-);
-
-export const options = {
+  Legend,
+)
+const options = {
   responsive: true,
   plugins: {
     legend: {
@@ -36,25 +35,33 @@ export const options = {
 };
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
+const data = {
   labels,
   datasets: [
     {
       label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => 3),
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
     {
       label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => 4),
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
-  ],
+  ]
 };
-
-export function Grafico() {
-  return <Line options={options} data={data} />;
+function Grafico() {
+  return(
+    <div>
+      
+      <Line
+      options={options}
+        data={data}
+      />
+    </div>
+  )
 }
+
+export default Grafico
