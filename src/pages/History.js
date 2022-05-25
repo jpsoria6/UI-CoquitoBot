3c4bfbeef46c8f7ee4f0477078e47ec0868d7934
jpsoria6 <jpsoria6@hotmail.com>
2022-05-24 22:16:07 -0300

    tables with datagrid by material

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,8 +1,43 @@
-import TablaHistorial from "../components/Historial";
 import Grid from '@mui/material/Grid';
 import NavbarCustom from '../components/NavbarCustom';
+import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
+import {enviroment} from '../enviroment'
+import React, { useState,useEffect } from 'react';
+
+
 
 const History = () => {
+
+    const columnsName = [
+        { field: 'id', headerName: 'Id', width: 130 },
+        { field: 'coinPair', headerName: 'Coin Pair', width: 200 },
+        { field: 'quantityToBuy', headerName: 'Margin', width: 70 },
+        { field: 'operation', headerName: 'Type', width: 70 },
+        { field: 'roe', headerName: '% ROE', width: 70 },
+    ]
+
+    const [operations,setOperations] = useState([])
+    
+    useEffect(() => {
+        let queryParams = {
+            userId : sessionStorage.getItem('userId'),
+        }
+        axios.get(enviroment.urlBaseBack+'/Operation', {params : queryParams})
+                .then(res => {
+                   let finalRows = res.data.map(r => (
+                            {
+                              id: r.orderId,
+                              coinPair :r.coinPair,
+                              quantityToBuy:  r.quantityToBuy,
+                              operation: r.operation,
+                              roe: r.finalROE
+                            }
+                        )
+                   )
+                   setOperations(finalRows)
+                })
+    },[])
     return(
         <div>
         <Grid container spacing={2}>
@@ -13,7 +48,14 @@ const History = () => {
         <div style={{margin:'5px'}}>
             
             <h1>Ultimas operaciones</h1>
-            <TablaHistorial/>
+            <React.StrictMode>
+                    <div style={{ height: 400, width: '100%', justifyContent:'center' }}>
+                        <DataGrid
+                            rows={operations}
+                            columns={columnsName}
+                        />
+                    </div>
+            </React.StrictMode>
         </div>
         </Grid>
         </Grid>
--- src/pages/Principal.js
+++ src/pages/Principal.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import BasicCard from '../components/CustomCard';
 import '../index.css';
+import {enviroment} from '../enviroment'
 import MediaCard from '../components/ChartCard';
-import Tabla from '../components/TableOperations';
 import Grid from '@mui/material/Grid';
 import NavbarCustom from '../components/NavbarCustom';
-
+import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
 
 function PantallaPrincipal(){
+
+    const [currentOperations,setCurrentOperations] = useState([])
+    
+    const columnsName = [
+        { field: 'id', headerName: 'Id', width: 130 },
+        { field: 'coinPair', headerName: 'Coin Pair', width: 200 },
+        { field: 'quantityToBuy', headerName: 'Margin', width: 70 },
+        { field: 'operation', headerName: 'Type', width: 70 },
+        { field: 'roe', headerName: '% ROE', width: 70 },
+    ]
+
+    useEffect(() => {
+        let queryParams = {
+            userId : sessionStorage.getItem('userId'),
+            pendings : true
+        }
+        axios.get(enviroment.urlBaseBack+'/Operation', {params : queryParams})
+                .then(res => {
+                   let finalRows = res.data.map(r => (
+                            {
+                              id: r.orderId,
+                              coinPair :r.coinPair,
+                              quantityToBuy:  r.quantityToBuy,
+                              operation: r.operation,
+                              roe: r.finalROE
+                            }
+                        )
+                   )
+                   setCurrentOperations(finalRows)
+                })
+    },[])
+
+
     return (
         <div>
         <Grid container spacing={2}>
@@ -24,7 +58,14 @@ function PantallaPrincipal(){
             </div>
             <div>
                 <h1>Operaciones Abiertas</h1>
-                <Tabla/>
+                <React.StrictMode>
+                    <div style={{ height: 400, width: '80%', justifyContent:'center' }}>
+                        <DataGrid
+                            rows={currentOperations}
+                            columns={columnsName}
+                        />
+                    </div>
+                </React.StrictMode>
             </div>
         </div>
         </Grid>
