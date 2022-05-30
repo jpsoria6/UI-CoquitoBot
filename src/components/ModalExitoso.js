66b8d6db9b2a61e399cd79f5c42d5dd0dac6ca0b
facuuguzman00 <facu11tuc@gmail.com>
2022-05-30 14:11:41 -0300

    operations finish

--- src/components/ModalExitoso.js
+++ src/components/ModalExitoso.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../styles/styleLogin.css'
-import CheckIcon from '@mui/icons-material/Check';
+import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
+import { Button } from '@mui/material';
 
 const ModalExitoso = (props) => {
     return (
-        <div>
-            <div className='caja'>
-                <CheckIcon color='success'/>
+        <div className='cajaGrande'>
+            <div className='cajaMediana'>
+                <div className='caja'>
+                    <DoneOutlineIcon sx={{ color: 'success', fontSize:70 }} className="cajaIcono"/>   
+                </div>
+                <h1> {props.tittle}</h1>
+                <p> <b> {props.message} </b></p>
+                <Button>Aceptar </Button>
             </div>
-            <h1> {props.tittle}</h1>
-            <p> {props.message} </p>
         </div>
     )
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://31a2-181-94-58-241.ngrok.io',
+      target: 'http://67f4-2803-9800-9442-b305-e9a1-b48d-cc3b-33.ngrok.io',
       changeOrigin: true,
     })
   );
